dd66162c8cd7a6470c0ed16168128c39a89145de
Andre Viallon <viallon.andre@gmail.com>
2021-05-16 13:49:03 +0200

    Init app and basic ts and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false,
+	theme: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1550px'
+		}
+	},
+	fontFamily: {
+		sans: ['Spartan', 'sans-serif']
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
