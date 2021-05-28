3a70598245498b4bdf2574fee241b5ab988a9cf7
Andre Viallon <viallon.andre@gmail.com>
2021-05-28 17:03:44 +0200

    add font sizes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false,
-	theme: {
-		screens: {
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-			'2xl': '1550px'
-		}
-	},
 	fontFamily: {
 		sans: ['Spartan', 'sans-serif']
+	},
+	fontSize: {
+		'xs': '.75rem',
+		'sm': '.875rem',
+		'tiny': '.875rem',
+		'base': '1rem',
+		'lg': '1.125rem',
+		'xl': '1.25rem',
+		'2xl': '1.5rem',
+		'3xl': '1.875rem',
+		'4xl': '2.25rem',
+		'5xl': '3rem',
+		'6xl': '4rem',
+		'7xl': '5rem',
 	},
 	variants: {
 		extend: {},
